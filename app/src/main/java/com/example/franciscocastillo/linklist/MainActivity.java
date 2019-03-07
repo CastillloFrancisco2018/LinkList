cab4ed72dd5562c73c4068ca10bed3aefa54d784
franciscocastillo <fran.castillop@icloud.com>
2019-03-07 10:38:09 -0600

    not working in Logcat

--- app/src/main/java/com/example/franciscocastillo/linklist/MainActivity.java
+++ app/src/main/java/com/example/franciscocastillo/linklist/MainActivity.java
@@ -6,10 +6,15 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity
 {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
-
+        LinkedList ll = new LinkedList();
+        ll.addFront(8);
+        ll.addFront(7);
+        ll.addFront(3);
+        ll.display();
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
